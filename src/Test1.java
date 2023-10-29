18acb726d03e31b5c4e2c8141be3ba76d33e0f9a
Lucky9052 <srilakshmiyadlapalli1@gmail.com>
2023-10-29 19:48:36 +0530

    first commit...

--- src/Test1.java
+++ src/Test1.java
@@ -1,15 +1,11 @@
 public class Test1 {
-    public static void main(String [] args) {
-        int number=13;
-        //Check if the number is divisible by 2 or not
-        if(number%2==0)
-        {
-            System.out.println("even number");
-        }
-        else
-        {
-            System.out.println("odd number");
+    public static void main(String[] args) {
+        String hungry = "yes";
+        if (hungry == "yes")
+            System.out.println("eat somthing..");
+        else {
+            System.out.println("do work....");
+
         }
     }
 }
-
